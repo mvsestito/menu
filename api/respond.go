871e59dfd69aae87f02e3f1cdb76f16b85781849
Michael Sestito <mvsestito@gmail.com>
2018-07-20 15:19:43 -0400

    add SendTextResp helper func for sending plaintext responses

--- api/respond.go
+++ api/respond.go
@@ -4,6 +4,17 @@ import (
 	"net/http"
 )
 
+// SendTextResp sends an http response to client with plaintext body
+func SendTextResp(w http.ResponseWriter, msg string, code int) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(code)
+
+	if msg != "" {
+		bytes := []byte(msg)
+		w.Write(bytes)
+	}
+}
+
 // SendJSONResp sends an http response to client with a JSON encoded body
 func SendJSONResp(w http.ResponseWriter, data []byte, code int) {
 	w.Header().Set("Content-Type", "application/json")
